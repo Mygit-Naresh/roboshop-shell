ba7024eea33f5c00ea5cbdbbd17277aa263c2f3f
naresh <Naresh@gmail.com>
2024-03-19 15:25:33 +0530

    add fi in catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -11,55 +11,55 @@ if [ $ID -ne 0 ]
 then echo -e "your not authorized to install"
 else echo -e "your a root user"
 fi
-validate(){
+VALIDATE(){
 if [ $1 -ne 0 ]
 then echo -e "$R $3 $N"
 else echo -e  "$G $2 $N"
 fi
 }
 dnf module disable nodejs -y &>> LOGFILE
-VALDIATE $? "Disabled nodejs" "Unsuccessfull"
+VALIDATE $? "Disabled nodejs" "Unsuccessfull"
 dnf module enable nodejs:18 -y &>> LOGFILE
-VALDIATE $? "Enabled nodejs" "Unsuccessfull"
+VALIDATE $? "Enabled nodejs" "Unsuccessfull"
 
 dnf install nodejs -y &>> LOGFILE
-VALDIATE $? "Installed nodejs"
+VALIDATE $? "Installed nodejs"
 
 useradd roboshop &>> LOGFILE
-VALDIATE $? "Created roboshop account Locally"
+VALIDATE $? "Created roboshop account Locally"
 
 id roboshop
-VALDIATE $? "Checking if roboshop id is created successfully or not"
+VALIDATE $? "Checking if roboshop id is created successfully or not"
 
 mkdir /app &>> LOGFILE
-VALDIATE $? "Created app folder"
+VALIDATE $? "Created app folder"
 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> LOGFILE
-VALDIATE $? "Downloaded catalogue.zip folder from S3"
+VALIDATE $? "Downloaded catalogue.zip folder from S3"
 
 cd /app &>> LOGFILE
-VALDIATE $? "Changed directory to /app"
+VALIDATE $? "Changed directory to /app"
 
 unzip /tmp/catalogue.zip &>> LOGFILE
-VALDIATE $? "unzip success"
+VALIDATE $? "unzip success"
 
 npm install &>> LOGFILE
-VALDIATE $? "Installed dependencies"
+VALIDATE $? "Installed dependencies"
 
 cp /root/roboshop-shell/catalogue.service  /etc/systemd/system/catalogue.service &>> LOGFILE
-VALDIATE $? "Copied catalogue.service to /etc/systemd/system/catalogue.service"
+VALIDATE $? "Copied catalogue.service to /etc/systemd/system/catalogue.service"
 
 systemctl daemon-reload &>> LOGFILE
-VALDIATE $? "daemon reloaded"
+VALIDATE $? "daemon reloaded"
 systemctl enable catalogue &>> LOGFILE
-VALDIATE $? "enable catalogue service"
+VALIDATE $? "enable catalogue service"
 systemctl start catalogue &>> LOGFILE
-VALDIATE $? "Started catalogue service"
+VALIDATE $? "Started catalogue service"
 systemctl status  catalogue &>> LOGFILE
 cp /root/roboshop-shell/mongo.repo  /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copied mongo.repo file to /etc/yum.repos.d/mongo.repo"
 dnf install mongodb-org-shell -y &>> LOGFILE
-VALDIATE $? "Mongodb client installed"
+VALIDATE $? "Mongodb client installed"
 mongo --host 172.31.29.97 </app/schema/catalogue.js
-VALDIATE $? "Conneted to mongodb host and loaded schema"
+VALIDATE $? "Conneted to mongodb host and loaded schema"
 systemctl status  catalogue
