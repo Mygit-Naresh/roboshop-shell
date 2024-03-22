dd4b94f1184d8ed629e8aed2ae06a9d306721604
naresh <Naresh@gmail.com>
2024-03-22 21:57:13 +0530

    create aws resource in shell

--- createaws.sh
+++ createaws.sh
@@ -2,7 +2,6 @@
 INSTANCES=("mongodb" "redis" "rabbitMQ" "mysql" "user" "catalogue" "payment" "shipping" "web")
 for INSTANCE  in "${INSTANCES[@]}"
 do
-echo $INSTANCE is creating
 if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
 then
 aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text
