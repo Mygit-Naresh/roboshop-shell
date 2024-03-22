15ca29ce8f6156aef010a63d8e4db957b4e110cf
naresh <Naresh@gmail.com>
2024-03-22 23:02:26 +0530

    create aws resource in shell

--- createaws.sh
+++ createaws.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 INSTANCES=("mongodb" "redis" "rabbitMQ" "mysql" "user" "catalogue" "payment" "shipping" "web")
+HOSTEDZONE="Z101265833JA5X90XBKK8"
 for INSTANCE  in "${INSTANCES[@]}"
 do
 if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
@@ -9,6 +10,27 @@ else
 aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text
 fi
 echo "$INSTANCE created in aws successfully"
+IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+aws route53 change-resource-record-sets --hosted-zone-id Z101265833JA5X90XBKK8 --change-batch
+'{
+    "Changes": [
+        {
+            "Action": "CREATE",
+            "ResourceRecordSet": {
+                "Name": ""$INSTANCE".eternaltrainings.online",
+                "Type": "A",
+                "TTL": 12,
+                "ResourceRecords": [
+                    {
+                        "Value": "$IPADDRESS"
+                    }
+                ]
+            }
+        }
+    ]
+}'
+
 done
 
 
