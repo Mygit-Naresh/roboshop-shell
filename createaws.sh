d20b5998e9848792840064290236d03dcec91e6c
naresh <Naresh@gmail.com>
2024-03-22 20:51:27 +0530

    create aws resource in shell

--- createaws.sh
+++ createaws.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 INSTANCES=("mongodb" "redis" "rabbitMQ" "mysql" "user" "catalogue" "payment" "shipping" "web")
 echo "${INSTANCES[@]}"
-for INSTANCE  in $INSTANCES
+for INSTANCE  in "${INSTANCES[@]}"
 do
-if [ $INSTANCE = "mongodb" ] && [ $INSTANCE = "redis" ] && [ $INSTANCE = "shipping" ]
+if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
 then
 aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e
 else
 aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e
 fi
 done
-echo "All $INSTANCE are created in aws successfully"
+echo "All "${INSTANCES[@]}" are created in aws successfully"
