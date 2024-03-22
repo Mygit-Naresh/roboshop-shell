3802f807e62151e26efe15cd162c9d6f0a366eea
naresh <Naresh@gmail.com>
2024-03-22 21:46:11 +0530

    create aws resource in shell

--- createaws.sh
+++ createaws.sh
@@ -5,9 +5,9 @@ do
 echo $INSTANCE is creating
 if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
 then
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]' --query 'Instances[0].PrivateIpAddress' --output text
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]' --query 'Instances[0].PrivateIpAddress' --output text
 else
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]' --query 'Instances[0].PrivateIpAddress' --output text
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]' --query 'Instances[0].PrivateIpAddress' --output text
 fi
 echo "$INSTANCE created in aws successfully"
 done
