8c76b7d8c0d712fcb76ff1b75357ccfa5d50a397
naresh <Naresh@gmail.com>
2024-03-22 21:33:06 +0530

    create aws resource in shell

--- createaws.sh
+++ createaws.sh
@@ -5,9 +5,9 @@ for INSTANCE  in "${INSTANCES[@]}"
 do
 if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
 then
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test01}]' 
 else
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE)"}]' --query 'Instances[0].PrivateIpAddress' --output text
 fi
+echo "${INSTANCES[@]} created in aws successfully"
 done
-echo "All "${INSTANCES[@]}" are created in aws successfully"
