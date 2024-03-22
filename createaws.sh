e4148c99ea16bc3f3974686688363725ac9b2df5
naresh <Naresh@gmail.com>
2024-03-22 17:49:08 +0530

    create aws resource in shell

--- createaws.sh
+++ createaws.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --count 2 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e
+INSTANCES=("mongodb" "redis" "rabbitMQ" "mysql" "user" "catalogue" "payment" "shipping" "web")
+echo "${INSTANCES[@]}"
+for INSTANCE  in $INSTANCES
+do
+if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
+then
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e
+else
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e
+done
