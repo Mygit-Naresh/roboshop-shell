086b6d23be37bc3f14b4252b57d92b2459f91cc2
naresh <Naresh@gmail.com>
2024-03-22 23:30:01 +0530

    create aws resource in shell

--- test_createrecords.sh
+++ test_createrecords.sh
@@ -1,5 +1,20 @@
-CHANGE_BATCH=$(Changes: - Action: CREATE ResourceRecordSet: Name: test.eternaltrainings.online Type: A TTL: 10 ResourceRecords: - Value: 172.21.0.1)
+# create_dns_record.yaml
+Changes:
+  - Action: CREATE
+    ResourceRecordSet:
+      Name: test.eternaltrainings.online
+      Type: A
+      TTL: 10
+      ResourceRecords:
+        - Value: 172.2.20.10
 
+#!/bin/bash
+
+# Variables
+HOSTED_ZONE_ID="Z101265833JA5X90XBKK8"
+YAML_FILE="create_dns_record.yaml"
+
+# Create DNS record using AWS CLI
 aws route53 change-resource-record-sets \
---hosted-zone-id Z101265833JA5X90XBKK8 \
---change-batch "$CHANGE_BATCH"
+--hosted-zone-id "$HOSTED_ZONE_ID" \
+--cli-input-yaml file://"$YAML_FILE"
