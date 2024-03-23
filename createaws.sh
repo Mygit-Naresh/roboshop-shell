314f307df8f429c60de32061da890ec86747e97f
naresh <Naresh@gmail.com>
2024-03-23 09:42:01 +0530

    aws resource creation

--- createaws.sh
+++ createaws.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 INSTANCES=("mongodb" "redis" "rabbitMQ" "mysql" "user" "catalogue" "payment" "shipping" "web")
 HOSTEDZONE="Z101265833JA5X90XBKK8"
+T2_INSTANCE_TYPE="t2.micro"
+T3_INSTANCE_TYPE="t3.small"
 for INSTANCE  in "${INSTANCES[@]}"
 do
 if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
 then
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t3.small --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text
+T2_IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $T2_INSTANCE_TYPE --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
 else
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text
+T3_IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $T3_INSTANCE_TYPE --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
 fi
-echo "$INSTANCE created in aws successfully"
-IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
-echo "$INSTANCE = $IPADDRESS"
+echo -e "$T2_IPADDRESS = $T2_INSTANCE_TYPE"
+echo -e  "$T3_IPADDRESS = $T3_INSTANCE_TYPE"
+
+done
+<<c
+if [ $T2_INSTANCE_TYPE =   ]
 
 aws route53 change-resource-record-sets \
---hosted-zone-id YOUR_HOSTED_ZONE_ID \
+--hosted-zone-id Z101265833JA5X90XBKK8 \
 --change-batch '
     {
         "Comment": "Creating a record set for roboshop projetc and domain eternaltraings.line"
@@ -32,6 +37,6 @@ aws route53 change-resource-record-sets \
     }
         '
 
-done
 
+c
 
