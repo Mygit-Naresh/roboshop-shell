e58dfb3a87b74306b2562bd84192e6efdfccf672
naresh <Naresh@gmail.com>
2024-03-23 09:51:48 +0530

    aws resource creation

--- createaws.sh
+++ createaws.sh
@@ -5,14 +5,13 @@ T2_INSTANCE_TYPE="t2.micro"
 T3_INSTANCE_TYPE="t3.small"
 for INSTANCE  in "${INSTANCES[@]}"
 do
+echo -e "Creating $INSTANCE"
 if [ $INSTANCE = "mongodb" ] || [ $INSTANCE = "redis" ] || [ $INSTANCE = "shipping" ]
 then
-T2_IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $T2_INSTANCE_TYPE --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
-else
 T3_IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $T3_INSTANCE_TYPE --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
+else
+T2_IPADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $T2_INSTANCE_TYPE --security-group-ids sg-09806393e77f11a3e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$INSTANCE"}]" --query 'Instances[0].PrivateIpAddress' --output text)
 fi
-echo -e "$T2_IPADDRESS = $T2_INSTANCE_TYPE"
-echo -e  "$T3_IPADDRESS = $T3_INSTANCE_TYPE"
 
 done
 <<c
